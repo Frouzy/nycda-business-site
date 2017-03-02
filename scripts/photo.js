a0897359796820415f071db049b6f9851f0c31db
chuday <chuday28@gmail.com>
2017-03-02 19:11:40 +0100

    Changes on background color and semantic

--- scripts/photo.js
+++ scripts/photo.js
@@ -3,10 +3,8 @@ $(".photo1").hide();
 $(".photo1").slideDown(4000);
 $("figure").hide();
 $("figure").fadeIn(4000);
- });
-$(document).loaded(function() {
-$("body").prompt("what would uou like yo eat?");
 });
+
 
 
 
