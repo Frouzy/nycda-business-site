940fa6825fdc222bac1b31963c2c6c2a12d24a04
chuday <chuday28@gmail.com>
2017-03-02 13:48:38 +0100

    Photo_page
    I created the photo gallery .

---
+++ scripts/photo.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+$(".photo1").hide();
+$(".photo1").slideDown(4000);
+$("figure").hide();
+$("figure").fadeIn(4000);
+ });
+$(document).loaded(function() {
+$("body").prompt("what would uou like yo eat?");
+});
+
+
+
+
