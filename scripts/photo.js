3573d985513e7b31f15f354a7cace04d3a06d2b2
chuday <chuday28@gmail.com>
2017-03-07 22:06:24 +0100

    background color - border radius  and  photos changed

--- scripts/photo.js
+++ scripts/photo.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 $(".navbar").hide();
 $(".navbar").slideDown(3000);
 $("img").hide();
-$("img").fadeIn(8000);
+$("img").fadeIn(4000);
+$(".row").hide();
+$(".row").fadeIn(10000);
  });
 
