044bda997a0366a8932b76e43902e4358e2947c2
Julian Aijal <julian.aijal@gmail.com>
2017-03-07 13:21:16 +0100

    Reservation options

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,3 +1,31 @@
+$(document).ready(function () {
+
+//Reservation
+
+    $("#inputName").click(function () {
+        this.value = "";
+    });
+    $("#inputMail").click(function () {
+        this.value = "";
+    });
+
+    $("#reservation").click(function () {
+        var name = $("#inputName").val();
+        var people = $("#inputPeople").val();
+        if (name == "" || name == "Name") {
+            alert("Please fill in you name");
+        } else if (people == "" || people == "Amount of people") {
+            alert("Please fill in the amount of people you want to make the reservation for");
+        } else {
+            alert("Thanks for you reservation, " + name);
+        }
+    });
+
+
+});
+
+//Google Maps
+
 function initMap() {
     var amsterdam = {lat: 52.3702, lng: 4.8952};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -12,3 +40,4 @@ function initMap() {
     var trafficLayer = new google.maps.TrafficLayer();
     trafficLayer.setMap(map);
 }
+
