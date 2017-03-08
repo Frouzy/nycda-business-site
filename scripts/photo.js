faf6ed52b180881e16aa42dafd7728618c46dac3
chuday <chuday28@gmail.com>
2017-03-08 18:27:12 +0100

    Video -  border line . added

--- scripts/photo.js
+++ scripts/photo.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
-$(".navbar").hide();
-$(".navbar").slideDown(3000);
+$(".page-head").hide();
+$(".page-head").slideDown(3000);
 $("img").hide();
 $("img").fadeIn(4000);
 $(".row").hide();
 $(".row").fadeIn(5000);
+$("h3").hide();
+$("h3").fadeIn(5000);
  });
 
